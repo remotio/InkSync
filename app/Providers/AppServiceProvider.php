a26051dd57477ecf1fdbd6321f38cf8d4f25e4a5
Yuma Yamamoto <remotio706@gmail.com>
2024-07-26 18:37:23 +0900

    install breeze

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \URL::forceScheme("https");
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
